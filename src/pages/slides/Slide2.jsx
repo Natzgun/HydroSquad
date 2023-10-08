558d97b51ebef37361f75122add6f75dc5b877cc
dnda <dndamc@gmail.com>
2023-10-08 15:23:10 -0500

    motion added

--- src/pages/slides/Slide2.jsx
+++ src/pages/slides/Slide2.jsx
@@ -1,9 +1,9 @@
-
+import { motion } from "framer-motion"
 const Slide2 = ({ data }) => {
   return (
 
     <div className='h-screen p-16 grid grid-cols-3 grid-rows-4 gap-6 break-words'>
-      <div className='row-span-2 bg-blue-500 text-white text-center text-2xl py-4 rounded-lg'>
+      <motion.div whileHover={{ scale: 1.1 }} className='row-span-2 bg-blue-500 text-white text-center text-2xl py-4 rounded-lg'>
         <div className="flex flex-col items-center justify-center h-full px-6">
           1<h1 className="font-bold">{data.titulo}</h1>
           <p className="font-semibold text-xl">
@@ -11,27 +11,27 @@ const Slide2 = ({ data }) => {
           </p>
         </div>
 
-      </div>
-      <div className='row-span-2 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>2
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} className='row-span-2 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>2
         <p>{data.contenido2}</p>
-      </div>
-      <div className='row-span-4 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} className='row-span-4 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>
         <div className="flex flex-col items-center justify-center h-full px-6">
           <p className="font-semibold text-2xl">
             {data.contenido3}
           </p>
         </div>
-      </div>
-      <div className='row-span-2 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} className='row-span-2 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>
         <div className="flex flex-col items-center justify-center h-full px-6">
           <p className="font-semibold text-2xl">
             {data.contenido4}
           </p>
         </div>
-      </div>
-      <div className='row-span-2 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>5
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} className='row-span-2 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>5
         <p>{data.contenido5}</p>
-      </div>
+      </motion.div>
     </div>
   )
 }
--- src/pages/slides/Slide4.jsx
+++ src/pages/slides/Slide4.jsx
@@ -1,4 +1,4 @@
-
+import { motion } from "framer-motion"
 import React from 'react'
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
@@ -9,15 +9,29 @@ function Slide4({ data }) {
   return (
 
     <div className='h-screen p-16 grid grid-cols-3 grid-rows-2 gap-6 break-words'>
-      <div className='row-span-1 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>
+      <motion.div whileHover={{ scale: 1.1 }} className='row-span-1 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>
         3d
-      </div>
-      <div className='row-span-2 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} className='row-span-2 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>
         <img src={data.imagen} alt="" />
-      </div>
-      <div className='row-span-1 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>{data.titulo}</div>
-      <div className='row-span-1 bg-blue-500 text-white text-center text-3xl py-4 rounded-lg'>{data.contenido1}</div>
-      <div className='row-span-1 bg-blue-500 text-white text-center text-3xl py-4 rounded-lg'>{data.contenido2}</div>
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} className='row-span-1 bg-blue-500 text-white text-center text-5xl py-4 rounded-lg'>
+        {data.titulo}
+        </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} className='row-span-1 bg-blue-500 text-white text-center text-3xl py-4 rounded-lg'>
+      <div className="flex flex-col items-center justify-center h-full px-6">
+          <p className="font-semibold text-2xl">
+            {data.contenido1}
+          </p>
+        </div>
+        </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} className='row-span-1 bg-blue-500 text-white text-center text-3xl py-4 rounded-lg'>
+      <div className="flex flex-col items-center justify-center h-full px-6">
+          <p className="font-semibold text-2xl">
+            {data.contenido2}
+          </p>
+        </div>
+      </motion.div>
 
     </div>
 
