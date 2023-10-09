4a5174f1091e0f97b181c925b6973600735d1296
Natzgun <itzlilouxx@gmail.com>
2023-10-08 22:43:31 -0500

    Test Earth

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,8 @@ import EarthCanvas from "../components/Earth/EarthCanvas"
 const HomePage = () => {
   return (
     <div className="bg-slate-950">
-      <EarthCanvas />
+      <h1>Optimized?</h1>
+      {/* <EarthCanvas /> */}
       {/* <div className="h-40 bg-gray-100"> */}
       {/*   Hello chupapi */}
       {/* </div> */}
